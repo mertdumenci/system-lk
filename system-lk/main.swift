95c6f7870764843a9a21b62ca7c2a3c4d520f134
Mert Dumenci <mert@dumenci.me>
2017-09-08 14:27:35 -0400

    Initial Commit

---
+++ system-lk/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  system-lk
+//
+//  Created by Mert Dumenci on 9/8/17.
+//  Copyright © 2017 Mert Dumenci. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
